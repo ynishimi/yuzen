ec3f20a4e1429dbdc4e6ad145a5bd2c53dd6b5d3
ynishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2023-01-04 01:44:43 +0900

    パーティクルを追加

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 let walker = [];
+let particle = [];
 let file;
 let coordinate = [];
 let count = 0;
+let countTemp = 0;
 
 function preload() {
   //座標ファイルを読み込み
@@ -20,37 +22,53 @@ function setup() {
   for(let i=0;i<file.length;i++)
   {
     walker[i] = new Walker(i);
+    particle[i] = new Particle(parseFloat(coordinate[i][0]), parseFloat(coordinate[i][1]));
   }
 }
 
 function draw() {
-  //薄く塗っていく(red,green,blue,alpha)
-  fill(0,10);
-  rect(0,0,960,540);
-  walker[count].draw();
+
+  //薄く背景を塗っていく(red,green,blue,alpha)
+  fill(0,5);
+  
+  rect(0,0,960,540);  
+  if (count % 90 == 0){  
+    countTemp = count;
+  } 
+  particle[count].draw();
+  walker[countTemp].draw();
   count++;
+}
+
+class Particle {
+  constructor(x,y) {
+    // x += random(-10,10);
+    // y += random(-10,10);
+    this.position = createVector(x, y);
+  }
+  draw(){
+    this.velocity = createVector(random(-30,30), random(-30,30));
+    this.position.add(this.velocity);
+
+    //円を表示
+    noStroke();
+    fill(255,100);
+    circle(this.position.x, this.position.y, 2.5);
+    circle(this.position.x, this.position.y, 2.5);
+  }
 }
 
 class Walker {
   constructor(numCoordinate) {
-    this.position = createVector(parseInt(coordinate[numCoordinate][0]), parseInt(coordinate[numCoordinate][1]));
+    this.position = createVector(parseFloat(coordinate[numCoordinate][0]), parseFloat(coordinate[numCoordinate][1]));
   }
 
   draw() {
-    // this.velocity = createVector(random(-2,2), random(-2,2));
-    // this.position.add(this.velocity);
-
-    //円を表示
-    // noStroke();
-    // fill(255,50);
-    // circle(this.position.x, this.position.y, 20);
-
-
     //画像の色をかえたり薄くしたりする
-    // tint(255,255,0);
+    tint(255,20);
     //画像を表示(x, y, width, height)
     // rotate();
-    image(img,this.position.x, this.position.y, 50, 50);
+    image(img,this.position.x, this.position.y, 100, 100);
   }
 }
 
